d1ff47b7f3050f0b7e2e2e0460e07e56266336ad
Majok-Bol <bolmajok001@gmail.com>
2025-08-03 20:05:34 +0300

    created increment/decrement counter page

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+//import useState from react
+
+import { useState } from "react";
+
+// import { useState } from "react";
+function App() {
+  let count = 0;
+  //initialize state
+  const [increment, setIncrement] = useState(count);
+  function handleIncrement() {
+    // count=count+1;
+    setIncrement(increment + 1);
+    if(increment>=10){
+      alert("Reached maximum value");
+      setIncrement(increment);
+    }
+  }
+  function handleDecrement() {
+    setIncrement(increment - 1);
+    if(increment===0){
+      alert("Reached minimum value");
+      setIncrement(increment);
+    }
+  }
+  return (
+    <>
+      <button id="increment" onClick={handleIncrement}>
+        Increment
+      </button>
+      <button id="decrement" onClick={handleDecrement}>
+        Decrement
+      </button>
+      <p>{increment}</p>
+    </>
+  );
+}
+export default App;
